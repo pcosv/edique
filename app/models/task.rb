eb5fa18824718df48a0b1acd6a536ba60f584b1a
pcosv <pcosv@cin.ufpe.br>
2017-05-19 09:39:06 -0300

    Criação migration participation_users

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
 	belongs_to :project
+	has_many :users, through: :participations_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
 	has_many :participations
 	has_many :projects, through: :participations
+	has_many :tasks, through: :participations_task
 end
