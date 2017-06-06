34b08b49efe4443a73f1d8ed78526958de1c5821
Bruno Barbosa <brunobt94@gmail.com>
2017-06-06 18:34:19 -0300

    Added :idle named scope for Task
    The :idle named scope in the Task model only return tasks that have no user assigned for it (no participation_user relation with this task).

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
 
 	has_many :participations_users
 	has_many :users, through: :participations_users
+
+	# scope :idle, -> { includes(:participations_users).where( :participations_users => { :user_id => nil} ) }
+	scope :idle, -> { left_outer_joins(:participations_users).where( participations_users: { id: nil} ) }
 end
