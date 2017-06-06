317a8cfff4658d143025dc6cae48464c0a8445ec
Bruno Barbosa <brunobt94@gmail.com>
2017-06-06 18:45:43 -0300

    Added :by_user named scope for Task
    The :by_user named scope in the Task model only return tasks that have the user passed as a parameter assigned for it (a participation_user relation with this task and the user passed).

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,4 +6,8 @@ class Task < ApplicationRecord
 
 	# scope :idle, -> { includes(:participations_users).where( :participations_users => { :user_id => nil} ) }
 	scope :idle, -> { left_outer_joins(:participations_users).where( participations_users: { id: nil} ) }
+
+	scope :by_user, lambda { |user|
+		left_outer_joins(:participations_users).where( participations_users: { user_id: user.id })
+	}
 end
