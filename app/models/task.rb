9fa1abba4405611ff57aa561a3617c141050a8ee
Bruno Barbosa <brunobt94@gmail.com>
2017-06-06 19:03:04 -0300

    Added :not_by_user named scope to Task
    The :not_by_user named scope in the Task model only return tasks that have at least a user assigned to it, but not the user passed as a parameter assigned (at least one participation_user relation with this task exist, but none with the the user passed).

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,10 +4,13 @@ class Task < ApplicationRecord
 	has_many :participations_users
 	has_many :users, through: :participations_users
 
-	# scope :idle, -> { includes(:participations_users).where( :participations_users => { :user_id => nil} ) }
 	scope :idle, -> { left_outer_joins(:participations_users).where( participations_users: { id: nil} ) }
 
 	scope :by_user, lambda { |user|
 		left_outer_joins(:participations_users).where( participations_users: { user_id: user.id })
+	}
+
+	scope :not_by_user, lambda { |user|
+		where.not(id: by_user(user)).where.not(id: idle)
 	}
 end
