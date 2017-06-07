fb3661098ce8f0d066d21defa85519aafab09aa8
Bruno Barbosa <brunobt94@gmail.com>
2017-06-06 22:43:01 -0300

    Initial view skeleton + First static files
    Set the projects index to be the initial path.
    Started working on the visual, configuring static css and js files and initial visual of the projects index.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :users
   #resources :tasks
-  resources :projects do
+  resources :projects, :path => '/' do
     resources :tasks
   end
+
+  # root to '#projects#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
