2dc090e454432e7636341819dbdc260bcac1c97b
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-06-12 17:26:09 -0300

    BugFix: Fixed ghost login cookies
    Cookies caused SessionHelperis_loggedin? to behave incorrectly

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,7 @@ module SessionsHelper
     @current_session_user ||= User.find_by(id: session[:user_id])
   end
   def logged_in?
-    session[:user_id] != nil
+    session[:user_id] != nil && current_user != nil
   end
 
   def is_admin_session
