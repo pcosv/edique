4f76f5d6d9ca3312403d5874341ffabcb778a612
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-06-13 16:15:59 -0300

    Implementado teste de GUI de  login

--- features/step_definitions/views_scenarios/entrar_steps_gui.rb
+++ features/step_definitions/views_scenarios/entrar_steps_gui.rb
@@ -5,3 +5,28 @@ end
 Given(/^eu não estou logado$/) do
   visit(logout_path)
 end
+
+
+Given(/^o usuário "([^"]*)" está cadastrado com email "([^"]*)" senha "([^"]*)"$/) do |user_name, email, password|
+  User.create(first_name: user_name, email: email, password: password)
+end
+
+When(/^eu vou à página de login$/) do
+  visit login_path
+end
+
+And(/^eu preencho o email "([^"]*)"$/) do |email|
+  fill_in 'session[email]', with: email
+end
+
+And(/^eu preencho a senha com "([^"]*)"$/) do |password|
+  fill_in 'session[password]', with: password
+end
+
+And(/^eu devo ver "([^"]*)" no indicador de usuário$/) do |user_name|
+  page.should(have_content(user_name))
+end
+
+And(/^eu clico no botão de submeter login$/) do
+  find('#login_button').click
+end
