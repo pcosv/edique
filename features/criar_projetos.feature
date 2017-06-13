f0b464964a612a96903b6a257e7853b2301fbc16
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-06-12 22:21:28 -0300

    Refatorados testes de criar projetos
    Adicionado teste auxiliar

--- features/criar_projetos.feature
+++ features/criar_projetos.feature
@@ -3,6 +3,14 @@ Feature: Criar projetos
 	I want to criar um novo projeto
 	So that possa  gerenciar a construção de um novo edital
 
+
+	@ignore
+	Scenario: Adicionar projeto (sistema)
+		Given o sistema está vazio
+#		And eu estou logado
+		When eu adiciono o projeto "Edital Amigos do Batman" no sistema
+		Then o sistema deve possuir um projeto chamado "Edital Amigos do Batman"
+
 	@ignore
 	Scenario: Adicionar projeto repetido (sistema)
 		Given o projeto "Edital Amigos do Batman" está no sistema
--- features/step_definitions/controllers_scenarios/criar_projetos_steps.rb
+++ features/step_definitions/controllers_scenarios/criar_projetos_steps.rb
@@ -1,27 +1,41 @@
-@projects = []
-Given(/^o projeto "([^"]*)" está no sistema$/) do |arg|
-  param_proj = {project: {name: arg}}
-  post '/projects', param_proj
-
+@project = []
+Given(/^o projeto "([^"]*)" está no sistema$/) do |project_name|
+  Project.create(name: project_name)
 end
 
-When(/^eu adiciono o projeto "([^"]*)" no sistema$/) do |arg|
-  @projects = Project.All
-  param_proj = {project: {name: arg}}
-  post '/projects', param_proj
+When(/^eu adiciono o projeto "([^"]*)" no sistema$/) do |project_name|
+  @project = Project.all.to_a
+  param_proj = {project: {name: project_name}}
+  post projects_path, param_proj
 end
 
+
+Then(/^o sistema deve possuir um projeto chamado "([^"]*)"$/) do |project_name|
+  if(!Project.find_by_name(project_name))
+    throw("O objeto não foi armazenado")
+  end
+end
 Then(/^o sistema não armazena nenhum projeto$/) do
-  projs = Project.All
-  assert compare_projs(projs, @projects)
+  if(!compare_projs(@project, Project.all.to_a))
+    throw("Nenhum objeto deveria ser armazenado. #{Project.count - @count} foram adicionados")
+  end
 end
 
 
 def compare_projs(p1, p2)
-  return false if p1.count != p2.count
-
-  equal = true
+  puts p1
+  puts p2
+  return false if (!p1 ^ !p2) || (p1.count != p2.count)
+  return true if(!p1 && !p2)
   p1.each_with_index do |proj, i|
-    equal = p1.attributes == p2.attributes
+    puts(p1)
+    puts(p1[i])
+    puts p1[i].attributes
+    return false if(p1[i].attributes != p2[i].attributes);
   end
+  return true
+end
+
+Given(/^o sistema está vazio$/) do
+
 end
